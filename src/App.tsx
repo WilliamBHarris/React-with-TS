8ad39f3e088b008a559b9db800137c251170e535
Will Harris <bharris010486@gmail.com>
2022-01-09 19:05:32 -0500

    more work

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import List from './components/List'
+
+interface IState {
+  people: {
+    name: string;
+    age: number;
+    url: string;
+    note?: string;
+  }[];
+}
 
 function App() {
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: 'Lebron James',
+      url: 'https://cdn.nba.com/headshots/nba/latest/104x760/2544.png',
+      age: 36,
+      note: 'allergic to teams'
+
+    }
+  ]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List people={people} />
     </div>
   );
 }
---
+++ src/components/List.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+interface IProps {
+    people: {
+      name: string;
+      age: number;
+      url: string;
+      note?: string;
+    }[];
+  }
+  
+
+const List: React.FC<IProps> = ({people}) => {
+
+    const renderList = () => {
+        return people.map((person) => {
+            <li className='List'>
+                <div className='List-header'>
+                    <img className="List-img" src={person.url} alt='Player'/>
+                    <h2>{person.name}</h2>
+                </div>
+                <p>{person.age} years old</p>
+                <p className='List-note'>{person.note}</p>
+            </li>
+        })
+    }
+
+    return (
+        <ul>
+
+        </ul>
+    )
+}
+
+export default List
